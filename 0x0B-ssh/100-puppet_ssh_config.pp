27a53b67b7b78d1e6c435e6949894b48df677a35
Adeyemo <adeyemodanointed@yahoo.com>
2022-09-19 06:29:26 -0700

    another correcton

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,9 @@
 #Puppet config to change ssh_config
 
-class { 'ssh::server':
+file {'/etc/ssh/ssh_config'
+  path => '/etc/ssh/ssh_config',
+
+class { '::ssh::sshd_config':
   options	=> {
     'HostKeys' => ['~/.ssh/school'],
     'PasswordAuthentication' => 'no',
