6ff9d91e0c0d07992bf565c4d78931791d882157
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-02-09 08:06:08 +0000

    restart nginx added to puppet file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #Fix the number of files open issue on nginx
 exec {'Fix the nofiles issue on nginx':
-  command  => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
+  command  => 'sudo sed -i "s/15/4096/" /etc/default/nginx; sudo service nginx restart',
   provider => shell,
 }
