6e4a8a6c896f13e21dac896525e15e1e8e01764b
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-02-10 00:07:53 +0000

    change to task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,10 @@
-# Change OS configurations for  user holberton
-exec {'Change OS Configuration for  holberton user':
-  command  => 'sudo adduser holberton --disabled-password',
+# change system wide limits
+exec { 'Change soft limit':
+  command  => 'sudo sed -i "s/holberton\ssoft.*/holberton\tsoft\tnofile\t10000/" /etc/security/limits.conf',
+  provider => shell,
+}
+
+exec { 'Change hard limit':
+  command  => 'sudo sed -i "s/holberton\shard.*/holberton\thard\tnofile\t100000/" /etc/security/limits.conf',
   provider => shell,
 }
